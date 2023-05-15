7a0c22e1a6db68b57d987cc4f7e0d75dd7ddb240
Sfer7 <forever_kaka@naver.com>
2023-05-15 10:03:48 +0900

    feat: Spring Security CORS 설정

--- src/main/java/com/capstone/interactive_novel/common/security/SecurityConfiguration.java
+++ src/main/java/com/capstone/interactive_novel/common/security/SecurityConfiguration.java
@@ -2,11 +2,19 @@ package com.capstone.interactive_novel.common.security;
 
 import com.capstone.interactive_novel.oauth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -35,5 +43,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .and()
                 .and()
                     .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:8080",
+                "http://interactive-novel-web.s3-website.kr.object.ncloudstorage.com",
+                "http://interactive-novel-creators-web.s3-website.kr.object.ncloudstorage.com/",
+                "http://interactive-novel-editor.s3-website.kr.object.ncloudstorage.com/"
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.OPTIONS.name()
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
