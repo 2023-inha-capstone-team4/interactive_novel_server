18d39b6b486068d6a3983c80cc06204642e153b5
Sfer7 <forever_kaka@naver.com>
2023-05-25 09:46:22 +0900

    Feat: Swagger 작성 - ReaderController

--- src/main/java/com/capstone/interactive_novel/common/document/SwaggerDocument.java
+++ src/main/java/com/capstone/interactive_novel/common/document/SwaggerDocument.java
@@ -8,7 +8,7 @@ public class SwaggerDocument {
 
         // readerSignIn
         public static final String readerSignInValue = "READER 로그인";
-        public static final String readerSignInNotes = "<b>이메일</b>과 <b>비밀번호</b>로 로그인을 시도. <br>성공 시 <b>JWT Access Token</b>과 <b>JWT Refresh Token</b>을 <u>헤더</u>로 전달한다.";
+        public static final String readerSignInNotes = "<b>이메일</b>과 <b>비밀번호</b>로 로그인을 시도. <br>성공 시 <b>JWT Access Token</b>과 <b>JWT Refresh Token</b>을 <u>header</u>로 전달한다.";
 
         // publisherSignUp
         public static final String publisherSignUpValue = "PUBLISHER 회원가입";
@@ -16,11 +16,11 @@ public class SwaggerDocument {
 
         // publisherSignIn
         public static final String publisherSignInValue = "PUBLISHER 로그인";
-        public static final String publisherSignInNotes = "<b>이메일</b>과 <b>비밀번호</b>로 로그인을 시도. <br>성공 시 <b>JWT Access Token</b>과 <b>JWT Refresh Token</b>을 <u>헤더</u>로 전달한다.";
+        public static final String publisherSignInNotes = "<b>이메일</b>과 <b>비밀번호</b>로 로그인을 시도. <br>성공 시 <b>JWT Access Token</b>과 <b>JWT Refresh Token</b>을 <u>header</u>로 전달한다.";
 
         // naverSignIn
         public static final String naverSignInValue = "NAVER OAuth 2.0 로그인";
-        public static final String naverSignInNotes = "네이버 OAuth 2.0 로그인 API를 통해 <b>authorization_code</b>와 <b>state</b>값을 얻어 전달함으로써 로그인. <br>성공 시 <b>JWT Access Token</b>과 <b>JWT Refresh Token</b>을 <u>헤더</u>로 전달한다.";
+        public static final String naverSignInNotes = "네이버 OAuth 2.0 로그인 API를 통해 <b>authorization_code</b>와 <b>state</b>값을 얻어 전달함으로써 로그인. <br>성공 시 <b>JWT Access Token</b>과 <b>JWT Refresh Token</b>을 <u>header</u>로 전달한다.";
 
         // oauthLocalSignIn
 
@@ -29,5 +29,19 @@ public class SwaggerDocument {
         public static final String emailAuthNotes = "회원가입 시 발급된 <b>UUID값</b>을 대조해 이메일 인증을 진행. <br>직접 호출하는 API가 아님. <br>성공 시 별도의 반환값은 없다.";
 
         // tokenRefreshRequest
+    }
+
+    public static class documentAboutReaderController {
+        // getAuthorRole
+        public static final String getAuthorRoleValue = "아마추어 작가 신청";
+        public static final String getAuthorRoleNotes = "작가 기능을 인증하기 위해 <b>READER</b>의 <b>JWT Access Token</b>을 <u>Authorization header</u>로 전달. <br>성공 시 별도의 반환값은 없다.";
+        
+        // modifyProfileImg
+        public static final String modifyProfileImgValue = "프로필 이미지 변경";
+        public static final String modifyProfileImgNotes = "프로필 이미지를 변경하기 위해 <b>READER</b>의 <b>JWT Access Token</b>을 <u>Authorization header</u>로 전달. <br> 성공 시 해당 유저 정보가 반환된다.";
+        
+        // viewReaderInfo
+        public static final String viewReaderInfoValue = "유저 정보 보기";
+        public static final String viewReaderInfoNotes = "본인의 유저 정보를 확인하기 위해 <b>READER</b>의 <b>JWT Access Token</b>을 <u>Authorization header</u>로 전달. <br> 성공 시 해당 유저 정보가 반환된다.";
     }
 }
--- src/main/java/com/capstone/interactive_novel/reader/controller/ReaderController.java
+++ src/main/java/com/capstone/interactive_novel/reader/controller/ReaderController.java
@@ -3,6 +3,8 @@ package com.capstone.interactive_novel.reader.controller;
 import com.capstone.interactive_novel.reader.domain.ReaderEntity;
 import com.capstone.interactive_novel.reader.dto.ReaderDto;
 import com.capstone.interactive_novel.reader.service.ReaderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,25 +12,30 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import static com.capstone.interactive_novel.common.document.SwaggerDocument.documentAboutReaderController.*;
+
 @Slf4j
+@Api(tags = "독자 관련 API")
 @RestController
 @RequestMapping("/reader")
 @RequiredArgsConstructor
 public class ReaderController {
     private final ReaderService readerService;
 
+    @ApiOperation(value = getAuthorRoleValue, notes = getAuthorRoleNotes)
     @PostMapping("/getAuthor")
-    public ResponseEntity<String> applyAuthorRole(@AuthenticationPrincipal ReaderEntity reader) {
-        return ResponseEntity.ok(readerService.applyAuthorService(reader));
+    public ResponseEntity<?> applyAuthorRole(@AuthenticationPrincipal ReaderEntity reader) {
+        return ResponseEntity.ok(readerService.applyAuthorService(null));
     }
 
+    @ApiOperation(value = modifyProfileImgValue, notes = modifyProfileImgNotes)
     @PostMapping("/modifyProfileImg")
     public ResponseEntity<?> modifyProfileImg(@AuthenticationPrincipal ReaderEntity reader,
                                               @RequestPart MultipartFile file) {
         return ResponseEntity.ok(readerService.modifyReaderProfileImg(reader, file));
     }
 
-    @GetMapping("/info")
+    @GetMapping("/info")@ApiOperation(value = viewReaderInfoValue, notes = viewReaderInfoNotes)
     public ResponseEntity<ReaderDto.view> viewReaderInfo(@AuthenticationPrincipal ReaderEntity reader) {
         return ResponseEntity.ok(readerService.viewReaderInfo(reader));
     }
