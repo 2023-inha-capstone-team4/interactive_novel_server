aa9f0e3de46ec0c04374004fecee5093a8851604
Sfer7 <forever_kaka@naver.com>
2023-06-03 18:38:32 +0900

    Fix: EntityManager가 멀티 스레드 상황에서 동시적으로 접근해 생기는 오류 방지

--- src/main/java/com/capstone/interactive_novel/common/config/QuerydslConfiguration.java
+++ src/main/java/com/capstone/interactive_novel/common/config/QuerydslConfiguration.java
@@ -5,16 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
 @Configuration
 public class QuerydslConfiguration {
 
     @PersistenceContext
-    private EntityManager entityManager;
+    private EntityManagerFactory entityManagerFactory;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         return new JPAQueryFactory(entityManager);
     }
 }
