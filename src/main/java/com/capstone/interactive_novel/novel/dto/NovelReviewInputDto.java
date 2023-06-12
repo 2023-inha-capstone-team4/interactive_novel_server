008b08ff5853b1024d0675b4f217561f8c1f3f20
Sfer7 <forever_kaka@naver.com>
2023-06-12 21:00:26 +0900

    Fix: NovelReviewInputDto 생성자 오류 수정

--- src/main/java/com/capstone/interactive_novel/novel/dto/NovelReviewInputDto.java
+++ src/main/java/com/capstone/interactive_novel/novel/dto/NovelReviewInputDto.java
@@ -1,10 +1,14 @@
 package com.capstone.interactive_novel.novel.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NovelReviewInputDto {
     String review;
     int novelScore;
